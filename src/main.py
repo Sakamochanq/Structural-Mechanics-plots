a72299938bceb8efc0c9eb5e06f20db87eb04930
Sakamochanq <sakamochan123@outlook.jp>
2025-10-22 01:59:08 +0900

    feat: 入力エラーをキャッチする

--- src/main.py
+++ src/main.py
@@ -3,8 +3,12 @@ import analysis.LoadDeflection as LoadDeflection
 if __name__ == "__main__":
 
     # クライアントからの入力受付
-    values = input("\n(a, b, E) > ")
-    a, b, E = map(float, values.split(","))
+    values = input("\n❯ ")
+    try:
+        a, b, E = map(float, values.split(","))
+    except ValueError:
+        print("\n\033[31mカンマ区切りで3つの数値を入力してください。（左から a, b, E）\033[0m\n")
+        exit(1)
     
     # 荷重・たわみ関係のプロット
     LoadDeflection.plot(a, b, E)

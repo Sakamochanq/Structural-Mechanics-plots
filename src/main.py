c86a663000759802540b68e64c2a39299f6b3b0c
Sakamochanq <sakamochan123@outlook.jp>
2025-10-21 23:29:51 +0900

    feat: 任意の数値を扱えるように

--- src/main.py
+++ src/main.py
@@ -2,9 +2,13 @@ import analysis.LoadDeflection as LoadDeflection
 
 if __name__ == "__main__":
 
-    a = 8.95376
-    b = 0.30596
-    E = 6.28782
+    # クライアントからの入力受付
+    values = input("(a, b, E) > ")
+    a, b, E = map(float, values.split(","))
+    
+    # a = 8.95376
+    # b = 0.30596
+    # E = 6.28782
 
     # 荷重・たわみ関係のプロット
     LoadDeflection.plot(a, b, E)

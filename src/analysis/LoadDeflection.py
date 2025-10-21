f4e09d3ce1ff4ca0b11bb0694f6ccfa5a9c7613d
Sakamochanq <sakamochan123@outlook.jp>
2025-10-21 23:02:06 +0900

    feat: 荷重・たわみ関係のグラフ実装

--- src/analysis/LoadDeflection.py
+++ src/analysis/LoadDeflection.py
@@ -2,21 +2,42 @@ import matplotlib.pyplot as plt
 import assets.lang as lang
 import numpy as np
 
-def plot(x, y):
+def plot(a, b, E):
 
     # フォント設定
     lang.set_font(font_name=None or lang.find_japanese_font())
+ 
+    # x 軸の範囲（0〜10）
+    x = np.linspace(0, 5)
 
-    # データの生成（仮）
-    load = np.linspace(0, x, 100)
-    deflection = (load / y) ** 2
+    # 5つの式
+    y1 = a*x + b
+    y2 = a*x
+    y3 = E*x
+    y4 = (E*1.05)*x
+    y5 = (E*0.95)*x
 
-    # プロットの作成
+    # グラフ作成
     plt.figure(figsize=(8, 6))
-    plt.plot(deflection, load, color='gray')
+
+    plt.plot(x, y1, color='gray', label='y = ax + b')
+    plt.plot(x, y2, color='blue', label='y = ax')
+    plt.plot(x, y3, color='red', label='y = Ex')
+    plt.plot(x, y4, color='red', label='y = 1.05Ex')
+    plt.plot(x, y5, color='red', label='y = 0.95Ex')
+
+    # テキストの生成
+    plt.text(x[-1], y1[-1], '  y = ax + b', color='gray', va='center')
+    plt.text(x[-1], y2[-1], '  y = ax',     color='blue', va='center')
+    plt.text(x[-1], y3[-1], '  E',     color='red', va='center')
+    plt.text(x[-1], y4[-1], '  +5%', color='red', va='center')
+    plt.text(x[-1], y5[-1], '  -5%', color='red', va='center')
+
+    # タイトル・軸
     plt.title('図1  荷重・たわみの関係')
-    plt.xlabel('たわみδ（mm）')
-    plt.ylabel('荷重（N）')
+    plt.xlabel('たわみ δ (mm)')
+    plt.ylabel('荷重 (N)')
     plt.grid(True)
+    # plt.legend()
     plt.tight_layout()
     plt.show()
--- src/main.py
+++ src/main.py
@@ -2,5 +2,9 @@ import analysis.LoadDeflection as LoadDeflection
 
 if __name__ == "__main__":
 
+    a = 8.95376
+    b = 0.30596
+    E = 6.28782
+
     # 荷重・たわみ関係のプロット
-    LoadDeflection.plot(5000, 20000)
+    LoadDeflection.plot(a, b, E)

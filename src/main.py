252b7e267f0bd0d9b291f89e3f14f828bebee705
Sakamochanq <sakamochan123@outlook.jp>
2025-10-22 01:48:37 +0900

    remove: 仮値の削除

--- src/main.py
+++ src/main.py
@@ -6,9 +6,5 @@ if __name__ == "__main__":
     values = input("\n(a, b, E) > ")
     a, b, E = map(float, values.split(","))
     
-    # a = 8.95376
-    # b = 0.30596
-    # E = 6.28782
-
     # 荷重・たわみ関係のプロット
     LoadDeflection.plot(a, b, E)

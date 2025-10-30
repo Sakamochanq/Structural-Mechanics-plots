83063d68a6f1b9d46c77938752d2b8b42ca0a093
Sakamochanq <sakamochan123@outlook.jp>
2025-10-31 00:21:47 +0900

    feat: whileでループ処理の実装
    * その他コンソールログの修正

--- src/main.py
+++ src/main.py
@@ -2,13 +2,21 @@ import analysis.LoadDeflection as LoadDeflection
 
 if __name__ == "__main__":
 
-    # クライアントからの入力受付
-    values = input("\n❯ ")
-    try:
-        a, b, E = map(float, values.split(","))
-    except ValueError:
-        print("\n\033[31mカンマ区切りで3つの数値を入力してください。（左から a, b, E）\033[0m\n")
-        exit(1)
-    
-    # 荷重・たわみ関係のプロット
-    LoadDeflection.plot(a, b, E)
+    while True:
+        
+        # クライアントからの入力受付
+        print("\n\033[36mカンマ区切りで3つの数値を入力してください。（左から a, b, E）\033[0m")
+        print("\033[37m※ ?exit で終了します。\033[0m")
+        
+        values = input("\n❯ ")
+        try:
+            if values.strip() == "?exit":
+                break
+            
+            a, b, E = map(float, values.split(","))
+        except ValueError:
+            print("\n\033[31mError::Invalidinput\033[0m\n")
+            continue
+
+        # 荷重・たわみ関係のプロット
+        LoadDeflection.plot(a, b, E)
